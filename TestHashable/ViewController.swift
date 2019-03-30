3be6712cdbe69239e355265dc43c073627df8919
k2terada <moonsocean@gmail.com>
2019-03-30 12:30:59 +0900

    Initial Commit

---
+++ TestHashable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHashable
+//
+//  Created by Katsuhiko Terada on 2019/03/30.
+//  Copyright © 2019 Katsuhiko Terada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
